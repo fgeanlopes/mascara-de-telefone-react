5bbe8071b67b2c732b1cd3ff03160f722ddd67a1
Gean Lopes <43907478+fgeanlopes@users.noreply.github.com>
2021-07-13 23:22:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import react, {useState, useCallback, useEffect} from 'react';
+import react, {useState, useCallback} from 'react';
 
 export default function Mascara_Telefone_React() {
     const [numero, setNumero] = useState('');
     const handleInputChange = useCallback((e)=>{
         let dig = e.target.value;
         dig = dig.replace(/\D/g,"").replace(/^(\d{2})(\d)/g,"($1) $2").replace(/(\d)(\d{4})$/,"$1-$2");
-
         setNumero(dig)
       },[numero]);
 
